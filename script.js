ccc2a476514d40badaa79cd22233ee7c769c0328
YaAlan1 <chill.alanus@gmail.com>
2024-08-29 14:36:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let students = ["Farxod", "Azim", "Davlat", "Jasur", "Daler", "doni"];
+
+students.forEach((a, b) => b.length - a.length);
+
+let longestName = students[0];
+
+console.log(longestName); 
